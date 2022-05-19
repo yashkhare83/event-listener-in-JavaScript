646af67a60428e0b6e518fc544813bc1a7a671a4
yashkharedew <yashk@dewsolutions.in>
2022-05-19 15:15:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Event Listeners in JavaScript
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/yashkhare17/pen/QWQvmwB](https://codepen.io/yashkhare17/pen/QWQvmwB).
+
---
+++ src/script.js
@@ -0,0 +1,33 @@
+// Toggle Hide or Show onclick event.
+
+let btn= document.getElementById('btnPrimary');
+btn.addEventListener("click",displayToggle);
+
+// function bgRed starts
+function displayToggle(){
+  let para= document.getElementById("para");
+  if(para.style.display!="none"){
+    para.style.display="none";
+  }
+  
+  else{
+    para.style.display="block";
+  }
+}
+
+// Mouseover or mouseout event
+
+var para= document.getElementById("para");
+para.addEventListener("mouseover", bgRed);
+function bgRed(){
+  para.style.background="red";
+  para.style.color="white";
+}
+
+var para= document.getElementById("para");
+para.addEventListener("mouseout", bgNormal);
+function bgNormal(){
+  para.style.background="white";
+  para.style.color="black";
+}
+
